bc2f432f86024f81c8326c527c5522c2208bbac8
Oskru <oskrupaa@gmail.com>
2023-04-26 15:16:08 +0200

    Create simple frontend UI for the API

---
+++ frontend/src/conponents/App.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from 'react';
+import ReadableResult from './ReadableResult/ReadableResult';
+import './App.css';
+
+function App() {
+  const [endpoint, setEndpoint] = useState('');
+  const [currency, setCurrency] = useState('');
+  const [date, setDate] = useState('');
+  const [quotation, setQuotation] = useState('');
+  const [result, setResult] = useState(null);
+  const [url, setUrl] = useState('');
+  const [error, setError] = useState('');
+  let averagesURL = `http://localhost:8000/api/averages/${currency}/${date}`;
+  let maxMinAveragesURL = `http://localhost:8000/api/max-min-averages/${currency}/${quotation}`;
+  let majorDifferencesURL = `http://localhost:8000/api/major-differences/${currency}/${quotation}`;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let response;
+      setError(''); // clear previous errors
+
+      if (endpoint === 'averages') {
+        response = await fetch(averagesURL);
+        setUrl(averagesURL);
+      } else if (endpoint === 'max-min-averages') {
+        response = await fetch(maxMinAveragesURL);
+        setUrl(maxMinAveragesURL);
+      } else if (endpoint === 'major-differences') {
+        response = await fetch(majorDifferencesURL);
+        setUrl(majorDifferencesURL);
+      }
+
+      const data = await response.json();
+      if (response.status >= 400) {
+        setError(data.message);
+      }
+
+      setResult(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="home-page">
+      <header className="header">
+        <h1 className="header__title">NBP data middleware</h1>
+        <h3 className="header__body">
+          Look and test the middleware by using options given below
+        </h3>
+      </header>
+      <form onSubmit={handleSubmit} className="form">
+        <label className="form__endpoint">
+          <div className="form__endpoint-text">Endpoint</div>
+          <select
+            className="select"
+            value={endpoint}
+            onChange={(e) => {
+              setEndpoint(e.target.value);
+              setResult(null); // clear the result after changing endpoint
+            }}
+          >
+            <option value="">Select an endpoint</option>
+            <option value="averages">Averages</option>
+            <option value="max-min-averages">Max/Min Averages</option>
+            <option value="major-differences">Major Differences</option>
+          </select>
+        </label>
+        <label className="form__currency">
+          <div className="form__currency-text">
+            Currency code &#40;ISO 4217&#41;
+          </div>
+          <input
+            className="textfield"
+            type="text"
+            value={currency}
+            onChange={(e) => setCurrency(e.target.value)}
+          />
+        </label>
+        {endpoint === 'averages' && (
+          <label className="form__date">
+            <div className="form__date-text">Date &#40;YYYY-MM-DD&#41;</div>
+            <input
+              className="textfield"
+              type="text"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </label>
+        )}
+        {endpoint === 'max-min-averages' || endpoint === 'major-differences' ? (
+          <label className="form__quotations">
+            <div className="form__quotations-text">
+              Quotations &#40;&#60;1;255&#62;&#41;
+            </div>
+            <input
+              className="textfield"
+              type="text"
+              value={quotation}
+              onChange={(e) => setQuotation(e.target.value)}
+            />
+          </label>
+        ) : null}
+        <div className="form__button">
+          <button
+            disabled={!endpoint}
+            className={endpoint ? 'but' : 'but but--disabled'}
+            type="submit"
+          >
+            {endpoint ? 'Submit' : 'Select an endpoint'}
+          </button>
+        </div>
+      </form>
+      {result && (
+        <div className="results">
+          <div className="results__endpoint">
+            <div className="results__endpoint-text">Endpoint</div>
+            <div className="results__url">{url}</div>
+          </div>
+
+          <div className="results__response">Response</div>
+          <div className="results__result">
+            <ReadableResult result={result} endpoint={endpoint} error={error} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/conponents/ReadableResult/ReadableResult.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import './ReadableResult.css';
+
+function ReadableResult(props) {
+  let endpoint = props.endpoint;
+
+  if (props.error) {
+    return (
+      <div className="container">
+        <div className="response">
+          <div className="response__title">ERROR</div>
+          <div className="response__body">{props.error}</div>
+        </div>
+      </div>
+    );
+  }
+
+  if (endpoint === 'averages')
+    return (
+      <div className="container">
+        <div className="response">
+          <div className="response__title">Currency code</div>
+          <div className="response__body">{props.result.code}</div>
+        </div>
+        <div className="response">
+          <div className="response__title">Date</div>
+          <div className="response__body">{props.result.date}</div>
+        </div>
+        <div className="response">
+          <div className="response__title">Average</div>
+          <div className="response__body">{props.result.average}</div>
+        </div>
+      </div>
+    );
+  if (endpoint === 'max-min-averages')
+    return (
+      <div className="container">
+        <div className="response">
+          <div className="response__title">Currency code</div>
+          <div className="response__body">{props.result.code}</div>
+        </div>
+        <div className="response">
+          <div className="response__title">Quotations</div>
+          <div className="response__body">{props.result.quotations}</div>
+        </div>
+        <div className="response">
+          <div className="response__title">Max</div>
+          <div className="response__body">{props.result.max}</div>
+        </div>
+        <div className="response">
+          <div className="response__title">Min</div>
+          <div className="response__body">{props.result.min}</div>
+        </div>
+      </div>
+    );
+  if (endpoint === 'major-differences')
+    return (
+      <div className="container">
+        <div className="response">
+          <div className="response__title">Currency code</div>
+          <div className="response__body">{props.result.code}</div>
+        </div>
+        <div className="response">
+          <div className="response__quotation-title">Quotations</div>
+          <div className="response__quotation-body">
+            {props.result.quotations}
+          </div>
+        </div>
+        <div className="response">
+          <div className="response__title">Major difference</div>
+          <div className="response__body">
+            {props.result.majorDifference.toFixed(5)}
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default ReadableResult;
